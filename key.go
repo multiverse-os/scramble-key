29e9a496b60dd4d5750d2ce8b6c4f6b8db372435
Your Name <you@example.com>
2018-11-22 14:13:20 -0300

    Building out the keyring concept and upgraded the key concept to better fit the scramble key design

--- key.go
+++ key.go
@@ -4,51 +4,92 @@ import (
 	"bytes"
 	"encoding/base32"
 	"encoding/json"
-	"fmt"
-	"os"
 	"strings"
+	"time"
 
-	io "github.com/multiverse-os/scramblekeys/atomicio"
-	"golang.org/x/crypto/ed25519"
-	"golang.org/x/crypto/sha3"
+	ed25519 "golang.org/x/crypto/ed25519"
+	sha3 "golang.org/x/crypto/sha3"
 )
 
-type KeyPair struct {
-	Address    string             `json:address`
-	PublicKey  ed25519.PublicKey  `json:public_key`
-	PrivateKey ed25519.PrivateKey `json:private_key`
+// Development NOTES
+// TODO:
+//   * Add the ability to generate a key form another key deterministically
+//   * Be able to verify child keys against any parent key
+//   * Use merkle tree to have piece-meal verification against the merkle root
+//   * Patricia tree based key lookup
+
+type KeyType int
+
+// By defining specific Session key (think certificate), we can have keys
+// issued for a session that even if hijacked have very limited permissions
+const (
+	Root KeyType = iota
+	Recovery
+	Session
+)
+
+type Ring struct {
+	RootKey *Key            `json:"root_key,omitempty"`
+	Keys    map[string]*Key `json:"keys"` // map[Key.Address]*Key
 }
 
-func WriteJSONKeyFile(keyFilePath string, keyPair KeyPair) {
-	if _, err := os.Stat(keyFilePath); os.IsNotExist(err) {
-		encodedKeyFile, _ := keyPair.encodeJSON("  ")
-
-		err = io.WriteFile(keyFilePath, []byte(encodedKeyFile), 0644)
-		if err != nil {
-			fmt.Println("Error: ", err)
-		} else {
-			fmt.Println("File successfully written.")
-		}
-	} else {
-		fmt.Println("File exists.")
-	}
+type Key struct {
+	ParentKey  Key                `json:"parent_key,omitempty"`
+	ChildKeys  map[string]Key     `json:"child_keys,omitempty"`
+	Address    string             `json:"address"`
+	PublicKey  ed25519.PublicKey  `json:"public_key"`
+	PrivateKey ed25519.PrivateKey `json:"private_key"`
+	Expires    time.Time          `json:"expires,omitempty"`
 }
 
-func (keyPair KeyPair) encodeJSON(indent string) (string, error) {
-	output, err := json.MarshalIndent(keyPair, "", indent)
-	if err != nil {
-		return "", err
-	}
-	return string(output), err
-}
-
-func GenerateKey() KeyPair {
-	publicKey, privateKey, _ := ed25519.GenerateKey(nil)
-	return KeyPair{
+func New(seed []byte, expiresAt time.Time) Key {
+	publicKey, privateKey, _ := ed25519.GenerateKey(seed)
+	return Key{
 		Address:    (GenerateAddress(publicKey)),
 		PublicKey:  publicKey,
 		PrivateKey: privateKey,
+		Expires:    expiresAt,
 	}
+}
+
+func GenerateKey() Key {
+	return New(nil, nil)
+}
+
+func DeterministicKey(seed []byte) Key {
+	return New(seed, nil)
+}
+
+func SessionKey(expiresAt time.Time) Key {
+	return New(nil, expiresAt)
+}
+
+func (self Key) DerivativeKey() Key {
+	return New(string(self.PrivateKey), nil)
+}
+
+func (self Key) String() string {
+	return string(self.JSON())
+}
+
+func (self Key) OnionAddress() string {
+	return (self.Address + ".onion")
+}
+
+func (self Key) PGP() string {
+	// TODO: Output a determinically generated PGP key based on any given key
+}
+
+func (self Key) RSA() string {
+	// TODO: Output a determinically generated SSH compatible RSA key based on any given key
+}
+
+func (self Key) JSON() []byte {
+	output, err := json.MarshalIndent(self, "", "  ")
+	if err != nil {
+		return []byte{}
+	}
+	return output
 }
 
 func GenerateAddress(publicKey ed25519.PublicKey) string {
