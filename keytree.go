5d06d65f8b306e27833052617de7c831148ae9fd
Your Name <you@example.com>
2018-11-22 13:19:30 -0300

    will need to build out the tree but may just use an existing one, regarldess should be added to README.md

---
+++ keytree.go
@@ -0,0 +1,14 @@
+package main
+
+type Tree struct {
+}
+
+type Node struct {
+}
+
+type Leaf struct {
+}
+
+// TODO: Sessions will auto-expire and can not be used for recovery
+type Session struct {
+}
